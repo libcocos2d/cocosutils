b15c4cb123594fc44d9aa21205c7a2deeb3bb563
martell <martellmalone@gmail.com>
2014-11-15 00:20:08 +0000

    handle help and version

--- cocos/__init__.py
+++ cocos/__init__.py
@@ -1,3 +1,33 @@
+import sys
+import getopt
+
+VERSION = "1.0"
+
+def usage():
+	print "Usage: " +  sys.argv[0] + " [OPTIONS]"
+	#todo: get usage from all the plugins
+	sys.exit(1)
+
+
 def main():
-    """Entry point for the application script"""
-    print("Call your main application code here")
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "version"])
+	except getopt.GetoptError, err:
+		# print help information and exit:
+		print(err) # will print something like "option -a not recognized"
+		sys.exit(-1)
+
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif o in ("-V", "--version"):
+			print VERSION
+			sys.exit(0)
+		else:
+			assert False, "unhandled option"
+			sys.exit(-1)
+
+	argc = len(sys.argv)
+	if argc == 0:
+		usage()
